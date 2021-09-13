00a1362f3d9a05d84f407cb3e373af33d2780067
Sara Evans <saraevs@gmail.com>
2021-09-13 23:40:15 +0100

    feat: extend schema

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 // imports
 const { ApolloServer } = require('apollo-server');
 
-// 1 - the type definition which defines a schema with one field
+// 1 - the type definition which defines the schema
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
 // 2 - implementation of the schema
 // structure is the same as the structure of the type definition
+// Every schema has 3 different root types that corrspond to operation types (Query, Mutation and Subscription)
+// The fields on these root types are called root fields and define the available API operations
+
+// this schema only has a single root field called info (type = query)
+// when sending a query, mutation or subscription to a graphQL API - they always need to start with a root field
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => null
   }
 }
 
