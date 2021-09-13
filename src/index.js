dcc28f6a39550512c612c951a304ec0a416799e9
Sara Evans <saraevs@gmail.com>
2021-09-13 23:05:34 +0100

    chore: add comments

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
+// imports
 const { ApolloServer } = require('apollo-server');
 
-// 1
+// 1 - the type definition which defines a schema with one field
 const typeDefs = `
   type Query {
     info: String!
   }
 `
 
-// 2
+// 2 - implementation of the schema
+// structure is the same as the structure of the type definition
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`
   }
 }
 
-// 3
+// 3 - schema and resolvers are bundles and passed to ApolloServer
+// ApolloServer is imported from apollo-server
+// this tells the server what operations are accepted
+// and how they should be resolved
 const server = new ApolloServer({
   typeDefs,
   resolvers,
